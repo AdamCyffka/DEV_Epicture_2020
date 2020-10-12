6001966c7860e2a287429a2518205680bf2bf5ba
Neo <adam.cyffka@epitech.eu>
2020-10-12 13:37:19 +0200

    remove isSignIn function

--- src/views/Profile.js
+++ src/views/Profile.js
@@ -1,9 +1,8 @@
 import React from 'react';
-import { Header, Body, Thumbnail, Container, Icon, Button, View } from "native-base";
-import { TouchableOpacity, ImageBackground, StatusBar, Dimensions, StyleSheet, Text, Platform, SafeAreaView } from 'react-native';
+import { Thumbnail, Icon, View } from "native-base";
+import { TouchableOpacity, ImageBackground, StatusBar, Dimensions, StyleSheet, Text, SafeAreaView } from 'react-native';
 import TopTabs from '../navigation/TopTabs';
 import AsyncStorage from '@react-native-community/async-storage';
-import { isSignedIn } from "../api/Imgur";
 
 async function getAccountInfo() {
   const token = await AsyncStorage.getItem('accessToken');
@@ -25,7 +24,6 @@ export default class Profile extends React.Component {
   constructor() {
     super()
     this.state = {
-      isSignIn: false,
       loading: true,
       avatar : null,
       background: null,
@@ -37,7 +35,6 @@ export default class Profile extends React.Component {
   }
 
   componentDidMount() {
-    if (isSignedIn()) this.setState({ isSignIn: true });
     getAccountInfo()
       .then((response) => {
         const data = response.data
@@ -54,18 +51,6 @@ export default class Profile extends React.Component {
   }
 
   render() {
-    if (!this.state.isSignIn) {
-      return (
-        <Container style={styles.centerContainer}>
-          <Button
-            style={styles.loginButton}
-            onPress={() => this.props.navigation.navigate("Welcome")}
-          >
-            <Text style={styles.loginButtonText}>LOGIN</Text>
-          </Button>
-        </Container>
-      );
-    }
     return (
       <SafeAreaView style={styles.container}>
         <StatusBar
@@ -81,16 +66,6 @@ export default class Profile extends React.Component {
           }}
           imageStyle={{ opacity: 0.8 }}
         >
-          <TouchableOpacity
-            onPress={() => this.props.navigation.navigate('Settings')}
-            style={{
-              position: "absolute",
-              right: 10,
-              top: Platform.OS === "ios" ? 34 : 10,
-            }}
-          >
-            <Icon name="settings" style={{ color: "white" }} />
-          </TouchableOpacity>
           <View>
             <View style={{ justifyContent: "center", alignItems: "center" }}>
               <Thumbnail source={{ uri: this.state.avatar }} />
@@ -104,36 +79,46 @@ export default class Profile extends React.Component {
               <Text style={styles.date}>
                 Created on {this.state.date.getMonth() + 1}/{this.state.date.getFullYear()}
               </Text>
+            <TouchableOpacity
+              onPress={() => this.props.navigation.navigate('Settings')}
+              style={{
+                position: "absolute",
+                right: 0,
+                marginRight: '3%',
+              }}
+            >
+              <Icon name="settings" style={{ fontSize: 35, color: "white" }} />
+            </TouchableOpacity>
             </View>
           </View>
         </ImageBackground>
         <TopTabs />
       </SafeAreaView>
-    );
+    )
   }
 }
 
-const width = Dimensions.get("window").width;
-const height = Dimensions.get("window").height;
+const width = Dimensions.get("window").width
+const height = Dimensions.get("window").height
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#191970',
+    backgroundColor: '#191970'
   },
   username: {
-    color: "white",
+    color: 'white',
     fontSize: 14,
     marginTop: 5,
-    fontWeight: "bold",
+    fontWeight: 'bold'
   },
   info: {
-    color: "white",
+    color: 'white',
     fontSize: 14,
     marginTop: 2,
-    marginBottom: 2,
+    marginBottom: 2
   },
   date: {
     color: 'white',
-    fontSize: 12,
-  },
-});
+    fontSize: 12
+  }
+})
