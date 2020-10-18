ddcd5ee8a19c7a7de74f16b8fd02a6b7236f1012
Neo <adam.cyffka@epitech.eu>
2020-10-18 15:07:12 +0200

    yaya

--- ios/EpimgurTests/EpimgurTests.m
+++ ios/EpimgurTests/EpimgurTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface epictureTests : XCTestCase
+@interface EpimgurTests : XCTestCase
 
 @end
 
-@implementation epictureTests
+@implementation EpimgurTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
