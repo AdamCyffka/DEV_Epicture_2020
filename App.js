c37086f20cc78987d82d716d3edfe7a942bcce61
Neo <adam.cyffka@epitech.eu>
2020-10-22 14:26:02 +0200

    remove useless libs

--- App.js
+++ App.js
@@ -3,11 +3,13 @@ import React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import { Provider as PaperProvider } from 'react-native-paper'
 import RootNavigator from './src/navigation/RootNavigator'
-
+import Toast from 'react-native-toast-message'
+ 
 export default function App() {
   return (
     <PaperProvider>
       <NavigationContainer>
+        <Toast ref={(ref) => Toast.setRef(ref)} />
         <RootNavigator />
       </NavigationContainer>
     </PaperProvider>
